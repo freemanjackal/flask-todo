3acc27fed7d116ad8e006486831ae13c3910efcf
freeman <freemanjackal@gmail.com>
2020-06-19 20:13:10 -0300

    fix details

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,  template_folder='../frontend/build/', static_folder='../frontend/build/static')
 
 JWT_SECRET_KEY = 'Rt1NP63m4wnBg6nyHJhyGtfsr23YKfmc2TpCOGI4nsCSs'
